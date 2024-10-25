875079409d87a0272085165d7ce16d094810e8eb
๑NaNa𐙚 ⋆ <164526857+NanaDoYgor@users.noreply.github.com>
2024-10-25 12:00:43 -0300

    Atualizacao_estilo

--- js/principal.js
+++ js/principal.js
@@ -21,13 +21,15 @@ for (var i = 0; i < pacientes.length; i++) {
     if (peso <= 0 || peso >= 1000) {
         console.log("Peso inválido!");
         pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.00) {
         console.log("Altura inválida!");
         alturaEhValida = false;
-        tdImc.textContent = "Altura inválida";
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
     }
 
   if (alturaEhValida && pesoEhValido) {
